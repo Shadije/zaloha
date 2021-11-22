e0f154f6959c37409741e0ce8e6f77f3981af97e
Shadije <90184867+Shadije@users.noreply.github.com>
2021-11-22 11:31:25 +0100

    Update trojuholnik.cpp

--- trojuholnik.cpp
+++ trojuholnik.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "trojuholnik.h"
+#include "inout.h"
 #include<cmath>
 
 using std::cout;
@@ -41,7 +42,8 @@ Bod Bod ::operator+( const Bod &other) const {
 Bod Bod ::operator*(const int cislo) const {
     return Bod(x*cislo, y*cislo);
 }
-Bod Bod :: operator *(int cislo, const Bod &other){
+
+Bod operator*(int cislo, const Bod &other) {
     return other*cislo;
 }
 
@@ -53,14 +55,14 @@ Bod Bod:: operator -(const Bod &other){
     return Bod(x-other.x, y-other.y);
 }
 
-bool Bod operator <(const Bod &other)
+bool Bod:: operator <(const Bod &other)
 {
-  return *this<other;
+    return *this<other;
 }
 
-bool Bod operator>(const Bod &other)
+bool Bod:: operator>(const Bod &other)
 {
-  return *this>other;
+    return *this>other;
 }
 
 float Bod:: getDistance (const Bod &other=Bod (0,0)) const{
@@ -75,44 +77,74 @@ Bod Bod:: getCenter (const Bod &other) const{
     return ((*this)+other)/2.f;
 }
 
-Bod Bod::operator^(constBod &other)const{
-  return this->getCenter(other);
+Bod Bod::operator^(const Bod &other)const{
+    return this->getCenter(other);
 }
 
 Bod::operator float() const {
     return this->getDistance(Bod(0,0));
 }
 
-void Bod::generujPoleBodov(Bod *pole, const int pocet) {
+void Bod::vygenerujPoleBodov(Bod *pole, const int pocet) {
     srand (time(NULL));
     Bod *poleBodov;
-  try{
-    pole = new Bod[pocet];
-    for (int i=0;i<pocet;++i) {
-        (pole + i)->x = 1 + std::rand() % 99;
-        (pole + i)->y = 1 + std::rand() % 99;
+    try{
+        pole = new Bod[pocet];
+        for (int i=0;i<pocet;++i) {
+            (pole + i)->x = 1 + std::rand() % 99;
+            (pole + i)->y = 1 + std::rand() % 99;
+        }
     }
-  }
-  catch (const std::bad_alloc &ex)
-  {
-    std::cout <<"Chybna alokacia pamate.";
-    return NULL;
-  }
-  return pole;
+    catch (const std::bad_alloc &ex)
+    {
+        std::cout <<"Chybna alokacia pamate.";
+        return NULL;
+    }
+    return pole;
 }
 
 void Bod::vypisPoleBodov(const Bod *pole, const int pocet) {
-  for(int i=0;i<pocet;++i){
-    cout<<*(pole+i)<<" ";
-  }
+    for(int i=0;i<pocet;++i){
+        cout<<*(pole+i)<<" ";
+    }
 }
 
 int Bod::cmp(const void *a, const void *b) {
     Bod *A=(Bod*)a;
     Bod *B=(Bod*)b;
-    
+
     return ((float)(*A)<(float)(*B))?1:((float)(*A)>(float)(*B))?-1:0;
 }
 
+void Bod::utriedPoleBodov(Bod *pole, const int pocet) {
 
-  
+}
+
+
+
+int Bod ::generujInt (int min, int max){
+    std::random_device rd; //ziska random cislo z harvera pocitaca
+    std::mt19937 //"osoli" generator
+
+}
+
+Priamka::Priamka() {
+    this->A=Bod();
+    this->B=Bod();
+
+}
+
+Priamka::Priamka(Bod a) {
+    this->A=a;
+    this->B=a;
+}
+
+Priamka::Priamka(Bod a, Bod b) {
+    this->A=a;
+    this->B=b;
+}
+
+std::ostream &operator<<(std::ostream &os, const Priamka &mojaPriamka) {
+    os<<"Primka: "<<mojaPriamka.A<<" "<<mojaPriamka.B<<endl;
+    return os;
+}
